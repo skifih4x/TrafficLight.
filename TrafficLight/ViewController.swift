2f885e4aa27b4e013a17ac1710db177708295e8e
artem <arte@kb.ri>
2022-02-04 19:54:04 +0300

    how change height and width  in swift

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,17 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet var redColorView: UIView!
     @IBOutlet var yellowColorView: UIView!
     @IBOutlet var greenColorView: UIView!
+    
     @IBOutlet var startButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         startButton.layer.cornerRadius = 10
+        
+        redColorView.alpha = 0.3
+        yellowColorView.alpha = 0.3
+        greenColorView.alpha = 0.3
+        
+        redColorView.layer.cornerRadius = redColorView.frame.size.height / 2
+        yellowColorView.layer.cornerRadius = yellowColorView.frame.size.height / 2
+        greenColorView.layer.cornerRadius = greenColorView.frame.size.height / 2
         
     }
     @IBAction func startButtonAction() {
+       
     }
     
 }
