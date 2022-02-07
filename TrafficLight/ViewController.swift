b53f2aa123347c4caba9b8c3d2be269c0ccba503
artem <arte@kb.ri>
2022-02-07 17:34:44 +0300

    ready!

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red, yellow, green
+}
 class ViewController: UIViewController {
     
     @IBOutlet var redColorView: UIView!
@@ -14,6 +17,8 @@ class ViewController: UIViewController {
     @IBOutlet var greenColorView: UIView!
     
     @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,14 +29,30 @@ class ViewController: UIViewController {
         yellowColorView.alpha = 0.3
         greenColorView.alpha = 0.3
         
-        redColorView.layer.cornerRadius = redColorView.frame.size.height / 2
-        yellowColorView.layer.cornerRadius = yellowColorView.frame.size.height / 2
-        greenColorView.layer.cornerRadius = greenColorView.frame.size.height / 2
-        
-    }
-    @IBAction func startButtonAction() {
-       
+        redColorView.layer.cornerRadius = redColorView.frame.width / 2
+        yellowColorView.layer.cornerRadius = redColorView.frame.width / 2
+        greenColorView.layer.cornerRadius = redColorView.frame.width / 2
     }
     
+    @IBAction func startButtonAction() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenColorView.alpha = 0.3
+            redColorView.alpha = 1
+            
+            currentLight = .yellow
+        case .yellow:
+            redColorView.alpha = 0.3
+            yellowColorView.alpha = 1
+            currentLight = .green
+        case .green:
+            greenColorView.alpha = 1
+            yellowColorView.alpha = 0.3
+            currentLight = .red
+        }
+    }
 }
-
